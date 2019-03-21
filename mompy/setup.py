897332e102ab9909f64da2db739fb8ba384c2400
Saran Ahluwalia <ahlusar.ahluwalia@gmail.com>
2019-03-21 14:12:41 -0400

    fix parse error

--- mompy/setup.py
+++ mompy/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='mompy',
-      version='0.1',
-      description='package for polymom'
-      author='sidaw',
-      author_email='sidaw@cs.stanford.edu',
+      version='0.2',
+      description='package for polymom',
+      author='sidaw and ahlusar1989',
+      author_email='sidaw@cs.stanford.edu, ahlusar.ahluwalia@gmail.com',
       license='MIT',
       zip_safe=False)
